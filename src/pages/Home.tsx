8c8400cd73601811c2ce45162f68a0b1d154ba57
Jody328 <jodyvermeulen007@gmail.com>
2025-06-07 16:25:43 +0200

    Fix margins on mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ export const Home = () => {
           />
         </span>
       </div>
-      <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+      <div className="min-[450px]:p-8 max-[450px]:p-4 md:p-12 lg:px-16 lg:py-24">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="text-2xl font-bold text-gray-900 md:text-3xl dark:text-white">
             Latest Prices
@@ -22,14 +22,14 @@ export const Home = () => {
         </div>
 
         <div className="mx-auto mt-8 max-w-xl">
-          <div className="flex flex-1 justify-center space-x-5">
-            <div className="flex-1 bg-radial-[at_25%_25%] from-[#398ab32d] to-[#16212e] to-55% border-1 border-[#4cb9f01f] rounded-xl p-4 flex justify-center mb-6 shadow-lg shadow-[#04040469]">
+          <div className="flex flex-1 justify-center min-[450px]:space-x-5">
+            <div className="min-[450px]:flex-1 max-[450px]:px-5 bg-radial-[at_25%_25%] from-[#398ab32d] to-[#16212e] to-55% border-1 border-[#4cb9f01f] rounded-xl p-4 flex justify-center mb-6 shadow-lg shadow-[#04040469]">
               <div className="flex-1 flex flex-col text-left justify-center">
-                <div className="flex-1.5 text-[#9fc7f9c2] max-[430px]:text-[12px] text-md">
+                <div className="flex-1.5 text-[#9fc7f9c2] max-[450px]:text-[12px] text-md">
                   Unleaded 95
                 </div>
-                <div className="flex-1 flex flex-row max-[450px]:flex-col items-baseline max-[450px]:items-start">
-                  <div className="max-[450px]:flex-0 flex-1 max-[450px]:text-3xl text-4xl max-[450px]:pb-2 font-bold text-[var(--color-blue-100)]">
+                <div className="flex-1 flex flex-row max-[620px]:flex-col items-baseline max-[450px]:items-start max-[620px]:gap-2">
+                  <div className="max-[450px]:flex-0 flex-1 max-[450px]:text-3xl text-4xl font-bold text-[var(--color-blue-100)]">
                     R 20.41
                   </div>
                   <div className="flex-1.5 inline-flex gap-2 rounded-sm p-1 bg-green-700 text-green-50">
@@ -53,15 +53,15 @@ export const Home = () => {
               </div>
 
               <div className="flex-0.5 px-3">
-                <div className="w-[0.5px] shadow-lg shadow-black bg-[#3c567338] border-l-[0.5px] border-[#10101000] h-18"></div>
+                <div className="w-[0.5px] shadow-lg shadow-black bg-[#3c567338] border-l-[0.5px] border-[#10101000] h-18 max-[450px]:h-21"></div>
               </div>
 
               <div className="flex-1 flex flex-col text-left justify-center">
-                <div className="flex-1.5 text-[#9fc7f9c2] max-[430px]:text-[12px] text-md">
+                <div className="flex-1.5 text-[#9fc7f9c2] max-[450px]:text-[12px] text-md">
                   Diesel
                 </div>
-                <div className="flex-1 flex flex-row max-[450px]:flex-col items-baseline max-[450px]:items-start">
-                  <div className="max-[450px]:flex-0 flex-1 max-[450px]:text-3xl text-4xl max-[450px]:pb-2 font-bold text-[var(--color-blue-100)]">
+                <div className="flex-1 flex flex-row max-[620px]:flex-col items-baseline max-[450px]:items-start max-[620px]:gap-2">
+                  <div className="max-[450px]:flex-0 flex-1 max-[450px]:text-3xl text-4xl font-bold text-[var(--color-blue-100)]">
                     R 17.81
                   </div>
                   <div className="flex-1.5 inline-flex gap-2 rounded-sm  p-1 bg-red-700 text-red-50">
