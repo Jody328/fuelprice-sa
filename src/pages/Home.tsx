021e46e6bb04d763bc2d3d6236f45365833b991f
Jody328 <jodyvermeulen007@gmail.com>
2025-06-07 02:09:41 +0200

    cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGasPump } from "@fortawesome/free-solid-svg-icons";
-
 export const Home = () => {
   return (
     <section className="flex flex-1 flex-col justify-center bg-gray-50 dark:bg-gray-900">
