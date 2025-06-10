86f9f77a742c9430bcabe57da345f126bb8806a4
Jody328 <jodyvermeulen007@gmail.com>
2025-06-10 23:56:06 +0200

    Fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Footer = () => {
   return (
-    <footer className="flex-[0.2] bg-gray-900">
+    <footer className="flex-[0.2] bg-gray-900 w-full">
       <div className="mx-auto max-w-screen-xl px-4 pb-8 sm:px-6 lg:px-8 lg:pt-24">
         <div className="mt-16 border-t pt-8 sm:flex sm:items-center sm:justify-between lg:mt-24 border-gray-800">
           <ul className="flex flex-wrap justify-center gap-4 text-xs lg:justify-end">
